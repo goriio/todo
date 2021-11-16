89fdb05b086d4a4c8381cc06743661cc2dd3a01a
goriio <gdsuaperoii@iskolarngbayan.pup.edu.ph>
2021-11-16 12:09:21 +0800

    install react-icons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import AddTodo from '../AddTodo/AddTodo';
 import { StyledHeader } from './Header.styles';
+import { BsSun } from 'react-icons/bs';
 
 const Header = () => {
   return (
     <StyledHeader>
       <h1>To-do List</h1>
+      <BsSun />
       <AddTodo />
     </StyledHeader>
   );
